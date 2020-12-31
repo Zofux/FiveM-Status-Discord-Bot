86b83632dda91394b44964337e1ce4e36b0badf2
Zofux <70540447+Zofux@users.noreply.github.com>
2020-12-31 20:59:45 +0100

    Fivem Status Bot

--- index.js
+++ index.js
@@ -3,27 +3,27 @@
 // DO NOT CHANGE THE CODE, change the config.json
 
 // CODE
-const Discord = require('discord.js');
-const bot = new Discord.Client()
-const fivereborn = require('fivereborn-query');
-const config = require('./config.json');
+const Discord = require('discord.js'); // Defines discord.js
+const bot = new Discord.Client() // Defines the client / the discord bot
+const fivereborn = require('fivereborn-query'); // Defines the npm library we use to get data from our fivem server
+const config = require('./config.json'); // Making so we can use the config.json as a config file
 
-bot.once('ready', () => {
-    console.log('Bot is online')
+bot.once('ready', () => { // A function that is run once when the bot is onlince
+    console.log('Bot is online') // Sends to the console that the bot is online
 })
 
-function activity(){
-    setTimeout(() => {
-        fivereborn.query(config.SERVER_IP,config.SERVER_PORT, (err, data) => { 
-            if (err) { 
-                console.log(err); 
-            } else { 
-                bot.user.setActivity(`${data.clients} players on ${config.SERVER_NAME}`, { type: "WATCHING" });
+function activity(){ // Defines the function
+    setTimeout(() => { // Starts a loop
+        fivereborn.query(config.SERVER_IP,config.SERVER_PORT, (err, data) => { // Starts a function that allowes us to retrive data from our fivem server
+            if (err) { // Checks for errors
+                return console.log(err); // If a error is true then this will log that error and then stop it from going by
+            } else { // If a error is not true then 
+                bot.user.setActivity(`${data.clients} players on ${config.SERVER_NAME}`, { type: "WATCHING" }); // Serts the Status
             }
         });
-        activity(); 
-    }, 1000);
+        activity(); // Runs the function we defined at line 15
+    }, 1000); // Waits 1 second
 }
-activity();
+activity(); // Runs the function again
 
-bot.login(config.BOT_TOKEN)
+bot.login(config.BOT_TOKEN) // Loging in to the bot
